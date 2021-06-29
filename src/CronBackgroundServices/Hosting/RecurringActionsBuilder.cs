13dcea454a2cd1aab79fa2e86d79dd2c09be5890
John Korsnes <johnkors@gmail.com>
2021-06-29 14:31:39 +0200

    dotnet 6 support (#2)
    * Makes recurring actions singletons instead of transient
    
    * dotnet 6 support
    
    +semver:major

--- src/CronBackgroundServices/Hosting/RecurringActionsBuilder.cs
+++ src/CronBackgroundServices/Hosting/RecurringActionsBuilder.cs
@@ -24,24 +24,24 @@ namespace CronBackgroundServices.Hosting
             if(!recurrers.Any())
                 throw new Exception("No recurrers added. Missing");
 
-            foreach (var recurrer in recurrers)
+            foreach(var recurrer in recurrers)
             {
-                Services.AddTransient<IHostedService>(s =>
+                Services.AddSingleton<IHostedService>(s =>
                 {
                     var allRecurrers = s.GetServices<IRecurringAction>();
                     var single = allRecurrers.First(r => r.GetType() == recurrer.ImplementationType);
                     var loggerFactory = s.GetService<ILoggerFactory>();
                     var logger = loggerFactory.CreateLogger(single.GetType());
-                    return new CronBackgroundService(single,logger);
-                }); 
+                    return new CronBackgroundService(single, logger);
+                });
             }
 
             return this;
         }
-        
+
         public IRecurringActionsBuilder AddRecurrer<T>() where T : class, IRecurringAction
         {
-            Services.AddSingleton<IRecurringAction,T>();
+            Services.AddSingleton<IRecurringAction, T>();
             return this;
         }
     }
