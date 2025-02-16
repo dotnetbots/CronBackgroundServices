a6b8d789156a32b6ed86df9784ba1ce0d22e5285
John Korsnes <johnkors@gmail.com>
2025-02-16 17:50:25 +0100

    .NET 9 (#11)
    deprecate .NET 7
    add .NET 9
    
    +semver:major

--- src/CronBackgroundServices/IRecurringAction.cs
+++ src/CronBackgroundServices/IRecurringAction.cs
@@ -5,29 +5,28 @@ namespace CronBackgroundServices;
 public interface IRecurringAction
 {
     /// <summary>
-    /// The job to be executed at intervals defined by the Cron expression
-    /// </summary>
-    /// <returns></returns>
-    Task Process(CancellationToken stoppingToken);
-
-    /// <summary>
-    /// The cron expression (including seconds) as defined by the Cronos library:
-    /// See https://github.com/HangfireIO/Cronos#cron-format
-    /// Ex: Every second: */1 * * * * *
-    /// Ex: Every minute: 0 */1 * * * *
-    /// Ex: Every midnight: 0 0 */1 * * *
-    /// Ex: First of every month 0 0 0 1 * *
+    ///     The cron expression (including seconds) as defined by the Cronos library:
+    ///     See https://github.com/HangfireIO/Cronos#cron-format
+    ///     Ex: Every second: */1 * * * * *
+    ///     Ex: Every minute: 0 */1 * * * *
+    ///     Ex: Every midnight: 0 0 */1 * * *
+    ///     Ex: First of every month 0 0 0 1 * *
     /// </summary>
     /// <returns>A valid Cron Expression</returns>
     string Cron { get; }
 
     /// <summary>
-    /// Optional: The TimeZone in which the Cron expression should be based on.
-    /// Defaults to UTC (Europe/London or GMT Standard Time)
-    ///
-    /// NB! When overriding this and targeting versions below .NET 6, use platform specific identifiers
-    /// If your runtime is .NET 6 or above, it's not required. It will handles the conversion:
-    /// See https://github.com/dotnet/runtime/pull/49412
+    ///     The job to be executed at intervals defined by the Cron expression
+    /// </summary>
+    /// <returns></returns>
+    Task Process(CancellationToken stoppingToken);
+
+    /// <summary>
+    ///     Optional: The TimeZone in which the Cron expression should be based on.
+    ///     Defaults to UTC (Europe/London or GMT Standard Time)
+    ///     NB! When overriding this and targeting versions below .NET 6, use platform specific identifiers
+    ///     If your runtime is .NET 6 or above, it's not required. It will handles the conversion:
+    ///     See https://github.com/dotnet/runtime/pull/49412
     /// </summary>
     /// <returns>timezoneId</returns>
     string GetTimeZoneId()
--- src/CronBackgroundServices/Timing.cs
+++ src/CronBackgroundServices/Timing.cs
@@ -2,14 +2,9 @@ using Cronos;
 
 namespace CronBackgroundServices;
 
-internal class Timing
+internal class Timing(string timeZoneId)
 {
-    public readonly TimeZoneInfo TimeZoneInfo;
-
-    public Timing(string timeZoneId)
-    {
-        TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-    }
+    public readonly TimeZoneInfo TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
 
     public DateTimeOffset RelativeNow(DateTimeOffset? nowutc = null)
     {
@@ -27,11 +22,13 @@ internal class Timing
         var expression = CronExpression.Parse(cron, CronFormat.IncludeSeconds);
         var fromUtc = DateTime.UtcNow;
         var upcoming = new List<DateTime>();
-        upcoming.AddRange(Get10Occurrences(upcoming, expression, fromUtc, fromUtc.AddMonths(1)));
+        upcoming.AddRange(GetTenNextsOccurrences(upcoming, expression, fromUtc, fromUtc.AddMonths(1)));
         return upcoming.Select(u => $"{u.ToLongDateString()} {u.ToLongTimeString()}");
     }
 
-    private IEnumerable<DateTime> Get10Occurrences(List<DateTime> upcoming, CronExpression expression, DateTime fromUtc, DateTime toUtc)
+    private IEnumerable<DateTime> GetTenNextsOccurrences(List<DateTime> upcoming, CronExpression expression,
+        DateTime fromUtc,
+        DateTime toUtc)
     {
         while (true)
         {
@@ -43,9 +40,10 @@ internal class Timing
             {
                 continue;
             }
+
             break;
         }
-        return upcoming.Take(10);
 
+        return upcoming.Take(10);
     }
 }
